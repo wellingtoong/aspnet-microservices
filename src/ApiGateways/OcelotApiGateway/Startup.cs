8dc991620201899ec90e1f554c2c75685ed668c3
Wellington Gonzalez <wellington.gonzalez@hotmail.com>
2023-07-30 13:54:36 -0300

    feat: added API gateway configuration for all APIs

--- src/ApiGateways/OcelotApiGateway/Startup.cs
+++ src/ApiGateways/OcelotApiGateway/Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Cache.CacheManager;
+
 
 namespace OcelotApiGateway
 {
@@ -14,7 +16,8 @@ namespace OcelotApiGateway
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddOcelot();
+            services.AddOcelot()
+                .AddCacheManager(settings => settings.WithDictionaryHandle());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
